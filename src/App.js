aea63ab27989f0fe6e011271f746ff63159bad97
Ankush Singh <ankushsahil45@gmail.com>
2022-04-22 20:31:38 +0530

    added spinner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Navbar from "./Components/Navbar";
+import { css } from "@emotion/core";
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 function App() {
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState(false);
+  const override = css`
+    display: block;
+    border-color: red;
+    margin-top: 10%;
+  `;
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+  return (
+    <div className="App">
+      loading ?{" "}
+      <PropagateLoader
+        color={"#3d2514"}
+        loading={loading}
+        css={override}
+        size={40}
+      />{" "}
+      :{" "}
+      <>
+        <Navbar />
+      </>
+    </div>
+  );
 }
 
 export default App;
