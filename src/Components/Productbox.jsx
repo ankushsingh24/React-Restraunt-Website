fb5d319c673cf5ac11533b0f3cb0e339125bb2e2
Ankush Singh <ankushsahil45@gmail.com>
2022-04-26 21:42:05 +0530

    product placed in box

--- src/Components/Productbox.jsx
+++ src/Components/Productbox.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function Productbox() {
+function Productbox(props) {
   return (
-    <div>Productbox</div>
-  )
+    <div className="a-box">
+      <div className="a-b-img">
+        <img src={props.image} alt="" />
+      </div>
+
+      <div className="a-b-text">
+        <h2>{props.title}</h2>
+        <button className="production-button">Order Now</button>
+      </div>
+    </div>
+  );
 }
 
-export default Productbox
+export default Productbox;
