70ba818f67a78e672345bfd1bf02cf07ddd56814
Ankush Singh <ankushsahil45@gmail.com>
2022-04-26 21:41:22 +0530

    added 2 images of burger

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Productbox from "./Productbox";
+import image1 from "../images/s1.png";
+import image2 from "../images/s2.png";
 
 function Product() {
   return (
-    <div>Product</div>
-  )
+    <div id="products">
+      <h1>CHOOSE & ENJOY</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Necessitatibus, ea.
+      </p>
+      <div className="a-container">
+        <Productbox image={image1} title="Luger Burger" />
+        <Productbox image={image2} title="Lu Pigeon Burger" />
+      </div>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
